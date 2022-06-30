4850286246247f707e03e10f9c2b1f7d641a6a74
Sander-Wauters <95096129+Sander-Wauters@users.noreply.github.com>
2022-06-30 14:05:39 +0200

    Update FirstWindowsProgram.cpp

--- C++/Windows/FirstWindowsProgram/FirstWindowsProgram.cpp
+++ C++/Windows/FirstWindowsProgram/FirstWindowsProgram.cpp
@@ -22,18 +22,18 @@ int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, 
 	const wchar_t WINDOW_NAME[] = L"WindowTest";
 
 	HWND hwnd = CreateWindowEx(
-		0,						// Optional window styles
-		CLASS_NAME,				// Window class
-		WINDOW_NAME,			// Window text
+		0,			// Optional window styles
+		CLASS_NAME,		// Window class
+		WINDOW_NAME,		// Window text
 		WS_OVERLAPPEDWINDOW,	// Window style
-		CW_USEDEFAULT,			// Position X
-		CW_USEDEFAULT,			// Position Y
-		CW_USEDEFAULT,			// Width
-		CW_USEDEFAULT,			// Height
-		NULL,					// Handle to parent
-		NULL,					// Menu
-		hInstance,				// Instance handle
-		NULL					// Additional application data
+		CW_USEDEFAULT,		// Position X
+		CW_USEDEFAULT,		// Position Y
+		CW_USEDEFAULT,		// Width
+		CW_USEDEFAULT,		// Height
+		NULL,			// Handle to parent
+		NULL,			// Menu
+		hInstance,		// Instance handle
+		NULL			// Additional application data
 	);
 
 	if (hwnd == NULL) // Check if CreateWindowEx FAILED
