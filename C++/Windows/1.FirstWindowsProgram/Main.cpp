f1a9ec41e53b0b185f12652d3b95895c82d6f6a5
Sander-Wauters <95096129+Sander-Wauters@users.noreply.github.com>
2022-07-22 15:16:01 +0200

    Update Main.cpp

--- C++/Windows/1.FirstWindowsProgram/Main.cpp
+++ C++/Windows/1.FirstWindowsProgram/Main.cpp
@@ -6,8 +6,8 @@
 
 int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ PWSTR lpCmdLine, _In_ int nCmdShow)
 {
+	// Create the main window
 	MainWindow mainWin;
-
 	if (!mainWin.Create(L"WindowTest", WS_OVERLAPPEDWINDOW))
 		return 0;
 
