25fd8a339f8fe28b7fb0df1b704d909ee43e5702
Sander-Wauters <95096129+Sander-Wauters@users.noreply.github.com>
2022-07-22 15:27:47 +0200

    Update MainWindow.h

--- C++/Windows/1.FirstWindowsProgram/MainWindow.h
+++ C++/Windows/1.FirstWindowsProgram/MainWindow.h
@@ -1,9 +1,23 @@
 #pragma once
+#pragma comment(lib, "comctl32.lib")
 #include "BaseWindow.h"
+#include <CommCtrl.h>
+
 
 class MainWindow : public BaseWindow<MainWindow>
 {
 public:
-	PCWSTR  ClassName() const { return L"Sample Window Class"; }
+	PCWSTR  ClassName() const { return L"MainWindowClass"; }
 	LRESULT HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+private:
+	BOOL CreateStatusBar(
+		PCWSTR lpWindowName,		 // Window name
+		DWORD dwStyle,				 // Window style
+		HWND hWndParent = 0,		 // Handle to parent
+		HMENU hMenu = 0				 // Handle to menu
+	);
+
+private:
+	HWND m_hwndStatusBar;
 };
